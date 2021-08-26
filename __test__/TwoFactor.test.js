ec4605b127970998c2f56bc59f34565945a377a8
claudiabdm <claudiabdm@hotmail.es>
2021-08-26 18:41:31 +0200

    Add some tests
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

---
+++ __test__/TwoFactor.test.js
@@ -0,0 +1,44 @@
+'use strict';
+import { expect } from '@jest/globals';
+import { spyOn } from 'jest-mock';
+import { TwoFactor } from '../TwoFactor.js';
+describe('Two Factor', () => {
+	const wrapper = document.createElement('div');
+	const twoFactor = new TwoFactor(wrapper, 4);
+	it('should render Two Factor', () => {
+		expect(twoFactor).toBeTruthy();
+    twoFactor.render();
+    expect(wrapper.children[0].className).toBe('code');
+	});
+
+	it('should be false if input is not number', () => {
+		const input = document.createElement('input');
+		input.value = 'abcd';
+		const inputInvalid = twoFactor.validateNumericInputs(input);
+		expect(inputInvalid).toBe(false);
+	});
+
+  it('should be input value if input is number', () => {
+		const input = document.createElement('input');
+		input.value = 1;
+		const inputValid = twoFactor.validateNumericInputs(input);
+		expect(inputValid).toBe('1');
+	});
+
+  it('should be first digit input value', () => {
+		const input = document.createElement('input');
+		input.value = 234;
+		const inputValid = twoFactor.validateNumericInputs(input);
+		expect(inputValid).toBe('2');
+	});
+
+  it('should call focus and select from element', () => {
+		const input = document.createElement('input');
+    spyOn(input, 'focus');
+    spyOn(input, 'select');
+		twoFactor.focusElement(input);
+		expect(input.focus).toHaveBeenCalled();
+    expect(input.select).toHaveBeenCalled();
+	});
+
+});
